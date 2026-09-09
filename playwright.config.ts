8b83bb16d485272a2fb39de280ae50ffd57d9be5
Dimmvvv <107928814+Dimmvvv@users.noreply.github.com>
2026-09-09 23:18:50 +0300

    ci: switch to clean multi-repo checkout strategy

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     
-    command: 'cd cypress-realworld-app && npx yarn dev',
+    command: 'cd ../cypress-realworld-app && yarn dev',
     
     url: 'http://localhost:3000',
     
