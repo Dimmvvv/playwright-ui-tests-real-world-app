cae62159ce08e327eb34583792d6ec6f91458d74
Dimmvvv <107928814+Dimmvvv@users.noreply.github.com>
2026-09-09 22:41:41 +0300

    feat: implement LoginPage POM and configure automated Playwright webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,9 +74,15 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    
+    command: 'cd ../cypress-realworld-app && yarn dev',
+    
+    // URL, по которому Playwright будет проверять, ожило ли приложение
+    url: 'http://localhost:3000',
+    
+    reuseExistingServer: !process.env.CI,
+    
+    timeout: 120 * 1000,
+  },
 });
--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,16 +1,22 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../page-objects/LoginPage';
 
 test.describe('UI Authentication Tests', () => {
 
-  test('should successfully log in with valid credentials', async ({ page }) => {
-    await page.goto('/signin');
-
-    await page.getByTestId('signin-username').locator('input').fill('Heath93');
-    await page.getByTestId('signin-password').locator('input').fill('s3cret');     
-    await page.getByTestId('signin-submit').click();
-
+  test('User should successfully log in with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('Heath93', 's3cret');
     await expect(page).toHaveURL('http://localhost:3000/');
     await expect(page.getByTestId('sidenav-username')).toBeVisible();
   });
-
+  test('Platform should show error message with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('Heath9312', 's3cret');
+    await expect(page).toHaveURL('http://localhost:3000/signin');
+    await expect(loginPage.errorMessage).toBeVisible();
+    await expect(loginPage.errorMessage).toContainText('Username or password is invalid');
+  });
 });
+
